20fa5e6610aee651af49c5261b85a91ea06ad897
Emma Grey <emgrey02@gmail.com>
2023-05-05 00:01:40 -0400

    Add more mobile stuff

--- picture-test/front-end.js
+++ picture-test/front-end.js
@@ -1,9 +1,13 @@
 let tab = document.querySelector('.form-toggle');
 let form = document.querySelector('.search');
+let formCnt = document.querySelector('.form-ctn');
+let btns = document.querySelector('.btn-ctn');
 
 tab.addEventListener('pointerdown', (e) => {
 	if (form.classList[1]) {
 		form.classList.remove('closed');
+		btns.classList.remove('hide');
+		formCnt.classList.add('boop');
 	} else {
 		form.classList.add('closed');
 	}
