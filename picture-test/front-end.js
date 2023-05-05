bd03817c2a4d16b0326a237036c3675bad9a5cad
Emma Grey <emgrey02@gmail.com>
2023-05-05 19:47:02 -0400

    Make mobile layout more intuitive

--- picture-test/front-end.js
+++ picture-test/front-end.js
@@ -2,6 +2,7 @@ let tab = document.querySelector('.form-toggle');
 let form = document.querySelector('.search');
 let formCnt = document.querySelector('.form-ctn');
 let btns = document.querySelector('.btn-ctn');
+let searchBtn = document.querySelector('.search-btn');
 
 tab.addEventListener('pointerdown', (e) => {
 	if (form.classList[1]) {
@@ -11,4 +12,25 @@ tab.addEventListener('pointerdown', (e) => {
 	} else {
 		form.classList.add('closed');
 	}
+});
+
+document.addEventListener('pointerdown', (e) => {
+	if (form.classList.length == 1) {
+		switch (e.target.localName) {
+			case 'body':
+			case 'header':
+			case 'canvas':
+			case 'h1':
+			case 'img':
+			case 'div':
+				form.classList.add('closed');
+				break;
+		}
+	}
+});
+
+searchBtn.addEventListener('pointerdown', (e) => {
+	setTimeout(() => {
+		form.classList.add('closed');
+	}, 300);
 });
