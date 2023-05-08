ac0e3306b042b7ab17ca6234bc46b9a8a475504f
UsrnGamesInc <71029215+Pi-Guy53@users.noreply.github.com>
2023-05-08 17:27:18 -0400

    working on connecting teleports, minor changes to the map atlas

--- picture-test/teleportDir.js
+++ picture-test/teleportDir.js
@@ -7,19 +7,55 @@ function generateTPMap() {
 
     //teleportArray.set(findNodeFromCoords(11, 13), findNodeFromCoords(15, 5)); //used with 32x32 files for tests
 
-    teleportArray.set(findNodeFromCoords(94, 379), findNodeFromCoords(95, 238));
-    teleportArray.set(findNodeFromCoords(95, 236), findNodeFromCoords(96, 101));
+    // teleportArray.set(findNodeFromCoords(94, 379), findNodeFromCoords(95, 238));
+    // teleportArray.set(findNodeFromCoords(95, 236), findNodeFromCoords(96, 101));
 
-    teleportArray.set(findNodeFromCoords(83, 417), findNodeFromCoords(74, 277));
-    teleportArray.set(findNodeFromCoords(74, 275), findNodeFromCoords(87, 129));
+    // teleportArray.set(findNodeFromCoords(83, 417), findNodeFromCoords(74, 277));
+    // teleportArray.set(findNodeFromCoords(74, 275), findNodeFromCoords(87, 129));
 
-    teleportArray.set(findNodeFromCoords(243, 372), findNodeFromCoords(253, 232));
-    teleportArray.set(findNodeFromCoords(253, 230), findNodeFromCoords(239, 102));
+    // teleportArray.set(findNodeFromCoords(243, 372), findNodeFromCoords(253, 232));
+    // teleportArray.set(findNodeFromCoords(253, 230), findNodeFromCoords(239, 102));
 
     // console.log('check TP');
     // console.log(tpContains(findNodeFromCoords(15, 34)));
     // console.log(tpContains(findNodeFromCoords(0, 0)));
     // console.log('/check TP');
+
+    //! Teleports (Stairs, Elevators, and Building connections) For the current Map atlas
+    //campus to arts
+    teleportArray.set(findNodeFromCoords(637, 1281), findNodeFromCoords(1860, 788));
+    teleportArray.set(findNodeFromCoords(661, 1280), findNodeFromCoords(1863, 877));
+    teleportArray.set(findNodeFromCoords(627, 1258), findNodeFromCoords(1958, 759));
+    teleportArray.set(findNodeFromCoords(626, 1222), findNodeFromCoords(2105, 760));
+    teleportArray.set(findNodeFromCoords(645, 1194), findNodeFromCoords(2219, 841));
+    teleportArray.set(findNodeFromCoords(678, 1234), findNodeFromCoords(2045, 935));
+
+    //campus to bateman
+    teleportArray.set(findNodeFromCoords(758, 1394), findNodeFromCoords(270, 792));
+    teleportArray.set(findNodeFromCoords(800, 1391), findNodeFromCoords(390, 770));
+    teleportArray.set(findNodeFromCoords(762, 1378), findNodeFromCoords(269, 662));
+    //bateman to phys-ed
+    teleportArray.set(findNodeFromCoords(314, 905), findNodeFromCoords(505, 667));
+    teleportArray.set(findNodeFromCoords(379, 820), findNodeFromCoords(505, 570));
+
+    //campus to west
+    teleportArray.set(findNodeFromCoords(715, 1497), findNodeFromCoords(73, 870));
+    teleportArray.set(findNodeFromCoords(714, 1479), findNodeFromCoords(43, 732));
+    teleportArray.set(findNodeFromCoords(753, 1480), findNodeFromCoords(138, 728));
+
+    //campus to science
+    teleportArray.set(findNodeFromCoords(719, 1462), findNodeFromCoords(450, 517));
+    teleportArray.set(findNodeFromCoords(730, 1440), findNodeFromCoords(525, 434));
+    teleportArray.set(findNodeFromCoords(675, 429), findNodeFromCoords(748, 1434));
+    teleportArray.set(findNodeFromCoords(384, 89), findNodeFromCoords(748, 1416));
+
+    //campus to work force training
+    teleportArray.set(findNodeFromCoords(440, 1506), findNodeFromCoords(131, 411));
+
+    //campus to phys-ed
+    teleportArray.set(findNodeFromCoords(0, 0), findNodeFromCoords(0, 0));
+    teleportArray.set(findNodeFromCoords(0, 0), findNodeFromCoords(0, 0));
+
 
     // Nodes get flaged if they are teleport nodes, and an end point is auto-generated
     let tempArr = Array.from(teleportArray.keys());
