5fa6a15d81feb5f5f91112f1aa79c43379ed4883
Emma Grey <emgrey02@gmail.com>
2023-05-08 20:17:31 -0400

    Make front-end like wireframe

--- picture-test/front-end.js
+++ picture-test/front-end.js
@@ -3,34 +3,40 @@ let form = document.querySelector('.search');
 let formCnt = document.querySelector('.form-ctn');
 let btns = document.querySelector('.btn-ctn');
 let searchBtn = document.querySelector('.search-btn');
+let text = document.querySelector('.text');
 
 tab.addEventListener('pointerdown', (e) => {
 	if (form.classList[1]) {
 		form.classList.remove('closed');
 		btns.classList.remove('hide');
-		formCnt.classList.add('boop');
+		text.textContent = 'close';
 	} else {
 		form.classList.add('closed');
+		text.textContent = 'open';
 	}
 });
 
-document.addEventListener('pointerdown', (e) => {
-	if (form.classList.length == 1) {
-		switch (e.target.localName) {
-			case 'body':
-			case 'header':
-			case 'canvas':
-			case 'h1':
-			case 'img':
-			case 'div':
-				form.classList.add('closed');
-				break;
+if (matchMedia('(hover: false)').matches) {
+	document.addEventListener('pointerdown', (e) => {
+		if (form.classList.length == 1) {
+			switch (e.target.localName) {
+				case 'body':
+				case 'header':
+				case 'canvas':
+				case 'h1':
+				case 'img':
+				case 'div':
+					form.classList.add('closed');
+					text.textContent = 'open';
+					break;
+			}
 		}
-	}
-});
+	});
+}
 
 searchBtn.addEventListener('pointerdown', (e) => {
 	setTimeout(() => {
 		form.classList.add('closed');
+		text.textContent = 'open';
 	}, 300);
 });
